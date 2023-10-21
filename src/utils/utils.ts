396991e6f99eeb73a33daafaf67e053909d6777c
serCJm <scjmoro@gmail.com>
2023-10-21 13:49:36 -0700

    Add new utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,29 +1,41 @@
 import { appendFile } from "fs/promises";
 
 export function randomNumber(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export function randomFloat(min: number, max: number, decimals: number) {
-    const str = (Math.random() * (max - min) + min).toFixed(decimals);
-    return parseFloat(str);
+	const str = (Math.random() * (max - min) + min).toFixed(decimals);
+	return parseFloat(str);
 }
 
 export function shuffleArr([...arr]: any[]) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
 }
 
 export async function logToFile(
-    filePath: string,
-    message: string
+	filePath: string,
+	message: string,
 ): Promise<void> {
-    try {
-        await appendFile(filePath, message);
-    } catch (error) {
-        console.error(`Error writing to file: ${error}`);
-    }
+	try {
+		await appendFile(filePath, message);
+	} catch (error) {
+		console.error(`Error writing to file: ${error}`);
+	}
+}
+
+export function getRandomIndex(length: number, exclude?: number): number {
+	let index;
+	do {
+		index = Math.floor(Math.random() * length);
+	} while (index === exclude);
+	return index;
+}
+
+export function getDeadline(minutes: number): number {
+	return Math.floor(Date.now() / 1000) + 60 * minutes;
 }
