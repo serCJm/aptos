b2e34432466715eb0d003fae71a9c6a358d22e13
serCJm <scjmoro@gmail.com>
2023-10-24 10:17:24 -0700

    Add liquidswap

--- MODULES_CONFIG.ts
+++ MODULES_CONFIG.ts
@@ -1,8 +1,20 @@
-import { ORDER } from "./src/types.js";
+import { LiquidSwap } from "./src/modules/liquidSwap.js";
+import { ModulesConfig, ORDER } from "./src/types.js";
+
+export const MODULE_MAP = {
+	LiquidSwap,
+} as const;
+
+export const MODULES_CONFIG: Record<string, ModulesConfig> = {
+	[LiquidSwap.name]: {
+		ENABLED: true,
+		EXCLUDED_TOKENS: [],
+	},
+};
 
 export const GLOBAL_CONFIG = {
-    PREVENT_SENDING_MAX_ETHER: false, // prevents from swapping entire eth balance if forget to provide amount in settings
-    MINMAX_WALLET_WAIT_TIME: [60 * 30, 60 * 60], // seconds
-    MINMAX_MODULES_WAIT_TIME: [60 * 3, 60 * 7], // seconds
-    ORDER: ORDER.RANDOM,
+	PREVENT_SENDING_MAX_ETHER: false, // prevents from swapping entire eth balance if forget to provide amount in settings
+	MINMAX_WALLET_WAIT_TIME: [60 * 30, 60 * 60], // seconds
+	MINMAX_MODULES_WAIT_TIME: [60 * 3, 60 * 7], // seconds
+	ORDER: ORDER.RANDOM,
 };
